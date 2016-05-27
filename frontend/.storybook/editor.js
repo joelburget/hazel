3fc0f2c4c83be0bfb39171a9a4704f6dd51e963c
Joel Burget <joelburget@gmail.com>
2016-05-26 22:51:27 -0400

    Add beginnings of a tuple.

--- frontend/.storybook/editor.js
+++ frontend/.storybook/editor.js
@@ -16,7 +16,6 @@ function contentFromPrimitive({ tag, contents }) {
       const text = '' + contents;
       return [{
         text,
-        type: 'unstyled',
         entityRanges: [{
           offset: 0,
           length: text.length,
@@ -27,7 +26,6 @@ function contentFromPrimitive({ tag, contents }) {
     case 'String':
       return [{
         text: contents,
-        type: 'unstyled',
         entityRanges: [{
           offset: 0,
           length: contents.length,
@@ -40,12 +38,48 @@ function contentFromPrimitive({ tag, contents }) {
   }
 }
 
+function contentFromValue({ tag, contents }) {
+  switch (tag) {
+    case 'Primitive':
+      return contentFromPrimitive(contents);
+
+    case 'Tuple':
+      const [linearity, tuplePieces] = contents;
+      let ret = [{
+        text: '(',
+        entityRanges: [{
+          offset: 0,
+          length: 1,
+          key: tag,
+        }]
+      }];
+      tuplePieces.forEach(piece => { ret = ret.concat(contentFromValue(piece)); });
+      ret = ret.concat([{
+          text: ')',
+          entityRanges: [{
+            offset: 0,
+            length: 1,
+            key: tag,
+          }]
+      }]);
+      return ret;
+
+    case 'Lam':
+    case 'Primop':
+    case 'Let':
+    case 'Index':
+    case 'Neu':
+    case 'Plus':
+    default:
+      throw Error('unhandled value tag');
+  }
+}
+
 function contentFromComputation({ tag, contents }) {
   switch (tag) {
     case 'FVar':
       return [{
         text: contents,
-        type: 'unstyled',
         entityRanges: [{
           offset: 0,
           length: contents.length,
@@ -53,17 +87,31 @@ function contentFromComputation({ tag, contents }) {
         }],
       }];
 
-    case 'Primitive':
-      return contentFromPrimitive(contents);
-
+    case 'BVar':
+    case 'App':
+    case 'Annot':
     default:
       throw Error('unhandled computation tag');
   }
-};
+}
 
-function convertFromComputation(computation) {
+function convertFromBlocks(blocks) {
+  let text = "";
+  let entityRanges = [];
+
+  blocks.forEach(block => {
+    entityRanges = entityRanges.concat(block.entityRanges);
+    // Tricky
+    entityRanges[entityRanges.length - 1].offset = text.length;
+    text = text + block.text;
+  });
+
   return convertFromRaw({
-    blocks: contentFromComputation(computation),
+    blocks: [{
+      text,
+      entityRanges,
+      type: 'unstyled',
+    }],
     entityMap: {
       FVar: {
         type: 'TOKEN',
@@ -74,6 +122,11 @@ function convertFromComputation(computation) {
         type: 'TOKEN',
         mutability: 'MUTABLE',
         data: {type: 'Primitive'},
+      },
+      Tuple: {
+        type: 'TOKEN',
+        mutability: 'MUTABLE',
+        data: {type: 'Tuple'},
       },
     },
   });
@@ -93,27 +146,39 @@ storiesOf('Editor', module)
     const content = convertFromRaw(rawContent);
     return <Editor onChange={onChange} content={content} />;
   }).add('(FVar "x")', () => {
-    const content = convertFromComputation({
+    const content = convertFromBlocks(contentFromComputation({
       tag: 'FVar',
       contents: 'x',
-    });
+    }));
     return <Editor onChange={onChange} content={content} />;
   }).add('(Primitive (Nat 5))', () => {
-    const content = convertFromComputation({
+    const content = convertFromBlocks(contentFromValue({
       tag: 'Primitive',
       contents: {
         tag: 'Nat',
         contents: 5,
       },
-    });
+    }));
     return <Editor onChange={onChange} content={content} />;
   }).add('(Primitive (String "hazel"))', () => {
-    const content = convertFromComputation({
+    const content = convertFromBlocks(contentFromValue({
       tag: 'Primitive',
       contents: {
         tag: 'String',
         contents: "hazel",
       },
-    });
+    }));
+    return <Editor onChange={onChange} content={content} />;
+  }).add('(Tuple Nonlinear (V.fromList [NatV 5, NatV 5]))', () => {
+    const content = convertFromBlocks(contentFromValue({
+      "tag":"Tuple",
+      "contents": [
+        "Nonlinear",
+        [
+          {"tag":"Primitive","contents":{"tag":"Nat","contents":5}},
+          {"tag":"Primitive","contents":{"tag":"Nat","contents":6}}
+        ]
+      ]
+    }));
     return <Editor onChange={onChange} content={content} />;
   });
